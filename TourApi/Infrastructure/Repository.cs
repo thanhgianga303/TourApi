13e48d12eb3d0964910b6eed3669dec7b905e53b
Giang coi <thanhgianga303@gmail.com>
2020-10-10 20:50:30 +0700

    add interface repository

---
+++ TourApi/Infrastructure/Repository.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TourApi.Data;
+
+namespace TourApi.Infrastructure
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly TourContext _context;
+        public Repository(TourContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+        public async Task<T> GetBy(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+        public async Task Add(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+        public async Task Update(int id, T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+        public async Task Delete(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
