63c30f897471fbfc9528c535b5d365a5036136e9
Giang coi <thanhgianga303@gmail.com>
2020-10-11 21:11:14 +0700

    update mapping

--- TourApi/Mapping/MappingProfile.cs
+++ TourApi/Mapping/MappingProfile.cs
@@ -15,14 +15,46 @@ namespace TourApi.Mapping
             CreateMap<CostDetailsDTO, CostDetails>();
             CreateMap<CostDetails, CostDetailsDTO>();
 
-            CreateMap<TourPrice, TourPriceDTO>();
-            CreateMap<TourPriceDTO, TourPrice>();
+            CreateMap<Customer, CustomerDTO>();
+            CreateMap<CustomerDTO, Customer>();
 
             CreateMap<Location, LocationDTO>();
             CreateMap<LocationDTO, Location>();
 
+            CreateMap<Hotel, HotelDTO>();
+            CreateMap<HotelDTO, Hotel>();
+
+            CreateMap<Job, JobDTO>();
+            CreateMap<JobDTO, Job>();
+
+            CreateMap<JobDetails, JobDetailsDTO>();
+            CreateMap<JobDetailsDTO, JobDetails>();
+
+            CreateMap<Staff, StaffDTO>();
+            CreateMap<StaffDTO, Staff>();
+
+            CreateMap<Tour, TourDTO>();
+            CreateMap<TourDTO, Tour>();
+
+            CreateMap<TourDetails, TourDetailsDTO>();
+            CreateMap<TourDetailsDTO, TourDetails>();
+
+            CreateMap<TourDetailsOfCustomer, TourDetailsOfCustomerDTO>();
+            CreateMap<TourDetailsOfCustomerDTO, TourDetailsOfCustomer>();
+
+            CreateMap<TourDetailsOfStaff, TourDetailsOfStaffDTO>();
+            CreateMap<TourDetailsOfStaffDTO, TourDetailsOfStaff>();
+
+            CreateMap<TouristGroup, TouristGroupDTO>();
+            CreateMap<TouristGroupDTO, TouristGroup>();
+
+            CreateMap<TourPrice, TourPriceDTO>();
+            CreateMap<TourPriceDTO, TourPrice>();
+
             CreateMap<TypesOfTourism, TypesOfTourismDTO>();
             CreateMap<TypesOfTourismDTO, TypesOfTourism>();
+
+
         }
     }
 }
