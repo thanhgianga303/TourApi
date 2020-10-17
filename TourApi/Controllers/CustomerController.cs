7a30253895d5067c218d1914d619db3d2edd683d
Giang coi <thanhgianga303@gmail.com>
2020-10-17 22:09:56 +0700

    add staffcontroller + customercontroller

---
+++ TourApi/Controllers/CustomerController.cs
@@ -0,0 +1,96 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TourApi.DTOs;
+using TourApi.Models;
+using TourApi.Models.IRepository;
+
+namespace TourApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ICustomerRepository _repository;
+        public CustomerController(IMapper mapper, ICustomerRepository repository)
+        {
+            _mapper = mapper;
+            _repository = repository;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CustomerDTO>>> GetAllCustomer()
+        {
+            var customers = await _repository.GetAll();
+            var customersDTO = _mapper.Map<IEnumerable<Customer>, IEnumerable<CustomerDTO>>(customers);
+            return Ok(customersDTO);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<CustomerDTO>>> GetCustomer(int id)
+        {
+            var customer = await _repository.GetBy(id);
+            var customerDTO = _mapper.Map<Customer, CustomerDTO>(customer);
+            return Ok(customerDTO);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCustomer(CustomerDTO customerDto)
+        {
+            var customer = _mapper.Map<CustomerDTO, Customer>(customerDto);
+            await _repository.Add(customer);
+            return CreatedAtAction(nameof(GetCustomer), new { id = customer.CustomerId }, customer);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCustomer(int id, CustomerDTO customerDto)
+        {
+            if (id != customerDto.CustomerId)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                var customer = _mapper.Map<CustomerDTO, Customer>(customerDto);
+                await _repository.Update(id, customer);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+
+            }
+            return NoContent();
+        }
+        private async Task<bool> CustomerExists(int id)
+        {
+            var customer = await _repository.GetBy(id);
+            if (customer != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            if (await CustomerExists(id))
+            {
+                await _repository.Delete(id);
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ TourApi/Controllers/StaffController.cs
@@ -0,0 +1,96 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TourApi.DTOs;
+using TourApi.Models;
+using TourApi.Models.IRepository;
+
+namespace TourApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class StaffController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IStaffRepository _repository;
+        public StaffController(IMapper mapper, IStaffRepository repository)
+        {
+            _mapper = mapper;
+            _repository = repository;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StaffDTO>>> GetAllStaff()
+        {
+            var staffs = await _repository.GetAll();
+            var staffsDTO = _mapper.Map<IEnumerable<Staff>, IEnumerable<StaffDTO>>(staffs);
+            return Ok(staffsDTO);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<StaffDTO>>> GetStaff(int id)
+        {
+            var staff = await _repository.GetBy(id);
+            var staffDTO = _mapper.Map<Staff, StaffDTO>(staff);
+            return Ok(staffDTO);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateStaff(StaffDTO staffDto)
+        {
+            var staff = _mapper.Map<StaffDTO, Staff>(staffDto);
+            await _repository.Add(staff);
+            return CreatedAtAction(nameof(GetStaff), new { id = staff.StaffId }, staff);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateStaff(int id, StaffDTO staffDto)
+        {
+            if (id != staffDto.StaffId)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                var staff = _mapper.Map<StaffDTO, Staff>(staffDto);
+                await _repository.Update(id, staff);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await StaffExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+
+            }
+            return NoContent();
+        }
+        private async Task<bool> StaffExists(int id)
+        {
+            var staff = await _repository.GetBy(id);
+            if (staff != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStaff(int id)
+        {
+            if (await StaffExists(id))
+            {
+                await _repository.Delete(id);
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
